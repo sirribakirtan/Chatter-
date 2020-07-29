b996cee4a1db413828bb2fa99d37001d03325227
Sirri Bakirtan <sirribakirtan13@gmail.com>
2020-07-29 14:41:06 +0300

    Validation Full Name and E-mail and take object from form.

---
+++ src/main/java/com/chatter/chatter/model/Person.java
@@ -0,0 +1,41 @@
+package com.chatter.chatter.model;
+
+public class Person {
+
+    private String fullName;
+    private String email;
+    private String password;
+    private String telephoneNumber;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+}
